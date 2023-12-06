c8bd478eec53e9369d3465b30c921de1567541a5
Elynn Wu <elynn510@gmail.com>
2023-12-06 15:32:15 -0800

    patch fix for trained model path

--- workflows/train-evaluate-scream-1yr/submit.sh
+++ workflows/train-evaluate-scream-1yr/submit.sh
@@ -10,7 +10,7 @@ source /usr/workspace/climdat/python_venv/miniconda3/envs/fv3net-dev/bin/activat
 train_config=training_config_test.yaml
 training_data=training_data.yaml
 trained_model_name=$(date +'%Y-%m-%d')-tq-test-$(openssl rand -hex 6)/no-tapering
-trained_model_output=trained_model_output/$trained_model_name
+trained_model_output=`pwd`/trained_model_output/$trained_model_name
 validation_data=val-data.yaml
 python -m fv3fit.train $train_config $training_data $trained_model_output --validation-data-config=$validation_data --no-wandb
 mv $SLURM_JOB_NAME-$SLURM_JOB_ID.out $trained_model_output
