34727094e367f4bc6528755fdfb589ff700e07b2
Elynn Wu <elynn510@gmail.com>
2023-12-19 22:10:35 -0800

    offline diag doc

---
+++ workflows/offline-diags/README.md
@@ -0,0 +1,8 @@
+offline-diags
+=============
+This workflow is used to run offline diagnostics on a trained ML model.
+
+To run the workflow on quartz or ruby:
+```
+sbatch submit.sh
+```
--- workflows/offline-diags/submit.sh
+++ workflows/offline-diags/submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 #SBATCH -p pdebug
 #SBATCH -J offline_compute
-#SBATCH -N 1
-#SBATCH -n 56
+#SBATCH -N 2
+#SBATCH -n 72
 #SBATCH -t 01:00:00
 #SBATCH -A mlgcrm
 #SBATCH --output=%x-%j.out
-trained_model=/usr/workspace/wu62/scream-docs/nudge-to-fine-scream-workflow/workflows/train-evaluate-scream-1yr/trained_model_output/2023-11-15-uv-tendencies/no-tapering
-test_data_config=test_data_tq_2010_1yr.yaml
-offline_output=offline_output_uv
-source /usr/workspace/wu62/miniconda3/envs/fv3net/bin/activate
-python -m fv3net.diagnostics.offline.compute $trained_model $test_data_config $offline_output --evaluation-grid=ne30
+trained_model=/usr/gdata/climdat/eamxx-ml/vcm-scream/sample-trained-ML-model/2023-12-13-tq-tendencies-5bba84c4e149/no-tapering
+test_data_config=test_data.yaml
+offline_output=`pwd`/$(date +'%Y-%m-%d')-offline-output-sample
+source /usr/workspace/climdat/python_venv/miniconda3/envs/fv3net-dev/bin/activate
+python -m fv3net.diagnostics.offline.compute $trained_model $test_data_config $offline_output --evaluation-grid=ne30 --catalog_path=/usr/WS1/climdat/eamxx-ml/fv3net/LC_catalog.yaml
 
 python -m fv3net.diagnostics.offline.views.create_report \
               $offline_output \
